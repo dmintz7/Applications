b7eacc7bfa9e0b38d1a6c5821c09a3376ff0fc3d
dmintz7 <dmintz37@gmail.com>
2021-01-24 23:34:37 -0500

    Update Log Format

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import config, os, sys, requests, socket, logging
 from logging.handlers import RotatingFileHandler
 
 
-filename, file_extension = os.path.splitext(os.path.basename(__file__))
-formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(module)15s:%(funcName)30s:%(lineno)5s - %(message)s')
+extra = {'folder_name': os.path.dirname(os.path.abspath(__file__)).split("/")[-1]}
+formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(folder_name)15s:%(module)15s:%(funcName)30s:%(lineno)5s - %(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 consoleHandler = logging.StreamHandler(sys.stdout)
@@ -14,6 +14,7 @@ logger.setLevel(config.LOG_LEVEL)
 fileHandler = RotatingFileHandler(config.LOG_FOLDER + '/Applications.log', maxBytes=1024 * 1024 * 1, backupCount=1)
 fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
+logger = logging.LoggerAdapter(logger, extra)
 
 apps = sorted(list(config.application_apps), key=lambda tup: tup[0])
 count=0
